37efe4f07a632b7c42f9afe11976f71894016cb4
Onur <onurtaademir@gmail.com>
2021-09-07 19:48:55 +0300

    Form Column has been added.

---
+++ lib/core/components/column/form_column.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class FormColumn extends StatelessWidget {
+  final List<Widget>? children;
+
+  const FormColumn({Key? key, this.children}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Spacer(flex: 1),
+        Expanded(
+          flex: 8,
+          child: Column(
+            children: children!,
+          ),
+        ),
+        Spacer(flex: 1),
+      ],
+    );
+  }
